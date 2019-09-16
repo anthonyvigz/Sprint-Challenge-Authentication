8065a4c422806b9ad39601a8f6e695d829696b08
anthonyvigz <anthonyvigz@gmail.com>
2019-09-16 19:34:42 -0400

    completed register function

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
 
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model.js');
+
+
+
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash;
+
+  Users.add(user)
+    .then(newUser => {
+      res
+        .status(201)
+        .json(newUser);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
